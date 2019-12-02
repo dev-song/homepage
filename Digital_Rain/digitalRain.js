e7a23562acb4de4fb86162f17bf372e859502c8a
dev-song <52951039+dev-song@users.noreply.github.com>
2019-12-02 22:21:46 +0900

    Add & Fix & Refactor
    ---------------------------------------
    Add:
    - Shadow on each character by CSS property
    
    Fix:
    - Click effect appears only on characters, not on Start/Stop button
    - When highlighted row returns to the top, bottom rows are naturally deemed (in previous version, they become instantly black as highlighted row returns)
    - Start/stop button becomes transparent with opacity 0.2 when the app is playing
    
    Refactor:
    - Huge functions are subdivided into simple functions
    > remove duplicated 'for' loops
    > simplify long 'if/else' if statements
    > replace hard coding with parameters

--- Digital_Rain/digitalRain.js
+++ Digital_Rain/digitalRain.js
@@ -79,10 +79,8 @@ var dataController = (function() {
                 html += '</div>';
 
                 // 'body' selector의 'beforeend' position에 html 삽입
-                document.querySelector('body').insertAdjacentHTML('beforeend', html);
+                document.querySelector('.wrapper').insertAdjacentHTML('beforeend', html);
             }
-
-            return html;
         },
 
         // obj에서 무작위 요소를 선택하고 textContent를 arr의 무작위 내용으로 바꿈
@@ -135,8 +133,8 @@ var UIController = (function() {
 
         // r'row_n'c'col_m'의 id를 가진 DOM 개체의 textContent를 Char로 표시
         displayCharacters: function(row_n, col_m, char) {
-            var elementId = '#r' + row_n + 'c' + col_m;
-            document.querySelector(elementId).textContent = char;
+            var elementId = 'r' + row_n + 'c' + col_m;
+            document.getElementById(elementId).textContent = char;
         },
 
         changeElemText: function(button, conditionVar, textA, textB) {
@@ -162,72 +160,37 @@ var UIController = (function() {
         },
 
         // 특정 column의 highlight를 현재 element의 아래 element로 지정
-        displayNewColumnHighlight: function(obj, col) {
-            var previousHighlightId;
+        displayNewHighlight: function(obj, col, hlRow) {
+            var previousHighlightId = 'r' + hlRow + 'c' + col;
             var newHighlightId;
 
-            for (var i = 1; i <= obj.rows; i++) {
-                var targetId = 'r' + i + 'c' + col;
-                var targetDOM = document.getElementById(targetId);
-
-                if (targetDOM.classList.contains('highlighted') && i < obj.rows) {
-                    previousHighlightId = targetId;
-                    newHighlightId = 'r' + (i + 1) + 'c' + col;
-                } else if (targetDOM.classList.contains('highlighted') && i === obj.rows) {
-                    // 각 column의 highlight된 row가 최하단이 되면 newHighlight를 1행으로 돌리는 조건문
-                    previousHighlightId = targetId;
-                    newHighlightId = 'r' + 1 + 'c' + col;
-                }
-            };
+            if (hlRow < obj.rows) {
+                newHighlightId = 'r' + (hlRow + 1) + 'c' + col;
+            } else if (hlRow === obj.rows) {
+                newHighlightId = 'r' + 1 + 'c' + col;
+            }
             
             document.getElementById(previousHighlightId).classList.remove('highlighted');
             document.getElementById(newHighlightId).classList.add('highlighted');
-
-            // console.log('element with the ID of ' + newHighlightId + ' is highlighted')
         },
 
-        // 특정 column의 highlight된 요소를 찾고 그 위는 밝게, 아래는 어둡게 하는 함수
-        ctrlColumnColor: function(obj, col) {
+        // 특정 열에서 Highlight된 row와의 간격에 따라 opacity을 조절하는 함수
+        ctrlColumnOpacity: function(obj, col, hlRow) {
             for (var i = 1; i <= obj.rows; i++) {
                 var targetId = 'r' + i + 'c' + col;
                 var targetDOM = document.getElementById(targetId);
 
-                if (targetDOM.classList.contains('highlighted')) {
-                    var colorClass = ['green_black', 'green_dark', 'green', 'green_light', 'green_white', 'black'];
-
-                    // highlighted 요소가 가리지 않도록 다른 클래스는 제외
-                    colorClass.forEach(function(val) {
-                        targetDOM.classList.remove(val);
-                    });
-
-                    // highlighted 요소로부터 거리에 따라 클래스 추가
-                    for (var j = 1; j <= obj.rows; j++) {
-                        var overId = 'r' + j + 'c' + col;
-
-                        // 클래스 추가 전 다른 클래스 제외
-                        colorClass.forEach(function(val) {
-                            document.getElementById(overId).classList.remove(val);
-                        });
-
-                        if (j < i - 13) {
-                            document.getElementById(overId).classList.add('black');
-                        } else if (j < i - 11) {
-                            document.getElementById(overId).classList.add('green_black');
-                        } else if (j < i - 9) {
-                            document.getElementById(overId).classList.add('green_dark');
-                        } else if (j < i - 6) {
-                            document.getElementById(overId).classList.add('green');
-                        } else if (j < i - 3) {
-                            document.getElementById(overId).classList.add('green_light');
-                        } else if (j < i) {
-                            document.getElementById(overId).classList.add('green_white');
-                        } else if (j > i) {
-                            document.getElementById(overId).classList.add('black');
-                        }
-                    }
+                // 현재 row가 hlRow 위일 경우 row마다 0.1씩 opacity 감소, 최저치는 0.1
+                // hlRow 아래라면 opacity = 0
+                if (hlRow < 10 && i > obj.rows - 10) {
+                    targetDOM.style.opacity = Math.max(0, 1 - (obj.rows + hlRow - i) * 0.1);
+                } else if (i - hlRow <= 0) {
+                    targetDOM.style.opacity = Math.max(0, 1 - (hlRow - i) * 0.1);
+                } else {
+                    targetDOM.style.opacity = 0;
                 }
             }
-        }
+        }    
     }
 })();
 
@@ -245,10 +208,12 @@ var appController = (function(dataCtrl, UICtrl) {
         // playing 변수를 조절하는 event listener를 Start 버튼에 할당
         document.getElementById('btn_play-stop').addEventListener('click', changePlayStatus);
 
-        document.querySelector('body').addEventListener('click', updateHighlight);
+        document.querySelector('.wrapper').addEventListener('click', function() {
+            updateHighlight(characterMatrix);
+        });
 
-        document.addEventListener('click', function() {
-            UICtrl.makeMultiPulseEffect(3, 250);
+        document.querySelector('.wrapper').addEventListener('click', function() {
+            UICtrl.makeMultiPulseEffect(3, 200);
         });
     };
 
@@ -256,41 +221,69 @@ var appController = (function(dataCtrl, UICtrl) {
     var changePlayStatus = function() {
         if (playing) {
             playing = false;
+            stopRepeat(repeatUpdate);
             document.getElementById('btn_play-stop').textContent = 'Start';
+            document.getElementById('btn_play-stop').style.opacity = 1;
+            document.getElementById('btn_play-stop').style.zIndex = 1;
         } else {
             playing = true;
+            repeatUpdate;
             document.getElementById('btn_play-stop').textContent = 'Stop';
+            document.getElementById('btn_play-stop').style.opacity = 0.2;
+            document.getElementById('btn_play-stop').style.zIndex = 10;
         }
         console.log('Play status changed; now playing: ' + playing);
     };
 
-    var setCharacterMatrix = function(element, position) {
-        dataController.getCharacterMatrix(characters, characterMatrix);
+    // 특정 열의 Highlight된 row를 찾는 함수
+    var findColHighlight = function(obj, col) {
+        var colHighlightRow;
 
-        // document.querySelector(element).insertAdjacentHTML(position, html);
-    };
+        for (var i = 1; i <= obj.rows; i++) {
+            var targetId = 'r' + i + 'c' + col;
+            var targetDOM = document.getElementById(targetId);
 
-    // 모든 column의 highlight를 다음으로 옮기고, highlight의 맞춰 주변 element의 색상을 update함
-    var updateHighlight = function() {
-        // column 별 update time에 쓰일 timeArray 생성
-        var delayTime = dataCtrl.getTimeArray(characterMatrix.columns);
-        // playing === true인 경우에만 update
-        if (playing) {
-            for (var i = 1; i <= characterMatrix.columns; i++) {
-                UICtrl.displayNewColumnHighlight(characterMatrix, i);
-                UICtrl.ctrlColumnColor(characterMatrix, i);
+            if (targetDOM.classList.contains('highlighted')) {
+                colHighlightRow = i;
             }
         }
+
+        return colHighlightRow;
+    };
+
+    // obj의 모든 column의 highlight를 다음으로 옮기고, highlight의 맞춰 주변 element의 색상을 update함
+    var updateHighlight = function(obj) {
+        // column 별 update time에 쓰일 timeArray 생성
+        // var delayTime = dataCtrl.getTimeArray(characterMatrix.columns);
+        document.querySelector('body').style.color = '#99ffbb'
+
+        for (var i = 1; i <= obj.columns; i++) {
+            var hlRow = findColHighlight(obj, i);
+            UICtrl.displayNewHighlight(obj, i, hlRow);
+            // highlight를 갱신했으므로 hlRow 변수를 재설정
+            hlRow = findColHighlight(obj, i);
+            UICtrl.ctrlColumnOpacity(obj, i, hlRow);
+        }
+    };
+
+    // setInterval 사용, 일정 주기로 update
+    var repeatUpdate = setInterval(function() {
+        updateHighlight(characterMatrix);
+    }, 250);
+    
+    // clearInterval로 setInterval 중지시키기
+    var stopRepeat = function(interval) {
+        clearInterval(interval);
     };
 
     return {
         init: function() {
-            setCharacterMatrix('body', 'beforeend');
+            dataCtrl.getCharacterMatrix(characters, characterMatrix);
             UICtrl.setInitialHighlight(characterMatrix);
-            updateHighlight();
 
-            // 200ms 간격으로 반복
-            setInterval(updateHighlight, 250);
+            // 일정 간격으로 반복
+            // repeatUpdate;
+            // setInterval(updateHighlight, 250);
             // setInterval(function() {
             //     dataCtrl.changeRndChar(characters, characterMatrix)
             // }, 50);

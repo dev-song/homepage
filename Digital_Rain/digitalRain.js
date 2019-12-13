0f948562d14a12ebd1fd63f4b0af8f2e520feb11
dev-song <52951039+dev-song@users.noreply.github.com>
2019-12-13 16:52:37 +0900

    Change: Directory name change
    ------------------------------------------
    > Change a directory name of finished project

--- Digital_Rain/digitalRain.js
+++ Digital_Rain/digitalRain.js
@@ -1,14 +1,5 @@
 // 글자데이터를 다루는 dataController, 화면에 나타내는 UIController, 앱 전체의 기능을 담당하는 appController로 구분
 
-// 프로그램의 기능-------------
-// Play 버튼 (or 페이지 진입 시) init() 함수가 실행되고, 프로그램이 동작
-// 각 row의 내용을 불러옴
-// 각 column별로 최초 시작지점을 random으로 정함
-// 각 row에서 i번째 column의 문자가 .current가 붙어있는지 확인
-    // 붙어있다면 문자를 하얀색으로 보여주기
-    // 안 붙어있다면 row가 current가 붙은 row보다 작을 경우 초록색으로
-    // 클 경우 검정색으로
-
 var playing;
 
 // 배열 내 무작위 항목을 선택하는 함수

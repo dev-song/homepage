b68a2b1d4aaf5598c68ad1b4687e8e47493ba956
dev-song <52951039+dev-song@users.noreply.github.com>
2019-12-27 23:37:11 +0900

    Fix: Get correct data for Lottery draw
    -----------------------------------------
    > Get draw date in a form of a string 'yyyymmdd'
    
    > Get draw result in a form of a list containing numbers

--- lottery_info/crawler/lottery_crawler.py
+++ lottery_info/crawler/lottery_crawler.py
@@ -1,10 +1,15 @@
 # Vietnam Vietlott MEGA 6/45 Lottery Result Crawler
 # Python crawler tutorial source: https://beomi.github.io/gb-crawling/posts/2017-01-20-HowToMakeWebCrawler.html
 # JSON data storing tutorial: https://opensource.com/article/19/7/save-and-load-data-python-json
+# JSON file reading & writing: Reading and Writing JSON to a File in Python
+# (https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/)
+# Python JSON module documentation: https://docs.python.org/3/library/json.html
 
-# import 'requests', 'beautifulsoup4' module
+# import 'requests', 'beautifulsoup4' module for data crawling
 import requests
 from bs4 import BeautifulSoup
+# import 'json' module for saving data
+import json
 
 # HTTP Get request
 req = requests.get('https://vietlott.vn/en/trung-thuong/ket-qua-trung-thuong/645')
@@ -32,17 +37,43 @@ html = BeautifulSoup(html_src, 'lxml')
 ### list 내 각각의 값은 객체로, .get('property name'), .text 등으로 활용 가능
 
 # get draw date from the website
-draw_date = html.select(
+draw_date_info = html.select(
     'div.chitietketqua_title > h5 > b:nth-child(2)'
 )
 
 # get draw result from the website
-draw_result = html.select(
+draw_result_info = html.select(
     'div.day_so_ket_qua_v2 > span'
 )
 
-for result in draw_result:
-    print(result.text)
+# seperate draw date to year/month/date
+draw_date = draw_date_info[0].text.split('/')
+draw_date.reverse()
+draw_date_yyyymmdd = ''.join(draw_date)
+print(draw_date_yyyymmdd)
 
-for date in draw_date:
-    print(date.text)
+# make a list of drawn numbers
+draw_result = []
+for result in draw_result_info:
+    draw_result.append(result.text)
+# print(draw_result)
+
+# make a dictionary to transfer information to JSON file
+draw_info = {}
+draw_info[draw_date_yyyymmdd] = draw_result
+# print(draw_info)
+
+with open('draw_record.json') as input_json:
+    # get data from existing JSON file
+    previous_data = json.load(input_json)
+
+    # sort data in JSON file
+
+    # check if draw_info == the latest data in JSON file and store the boolean to variable
+
+    # return the boolean above
+
+# open JSON file again, in 'write' mode
+    # if boolean was true, don't write data
+    # else, write data in JSON file
+        # json.dump(draw_info, output)
